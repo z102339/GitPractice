7d605e1ea82fad1df68f7b2868baadec9c082799
zjh <1023391675@qq.com>
2022-07-22 13:54:54 +0800

    add another hello

--- app/src/main/java/org/kcwx/gitdemo/MainActivity.java
+++ app/src/main/java/org/kcwx/gitdemo/MainActivity.java
@@ -3,6 +3,7 @@ package org.kcwx.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.tv).setOnClickListener((v)-> Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show());
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "another hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
